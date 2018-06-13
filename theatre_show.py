99a3980a82a7be2324a9191fb0d95f76ce3f518b
piyushsoni27 <piy1995s@gmail.com>
2018-06-14 02:59:53 +0530

    create tables for all the book links in MOVIE_Table

--- theatre_show.py
+++ theatre_show.py
@@ -18,6 +18,8 @@ show_table = pd.DataFrame(columns = show_cols)
 
 movie_table = pd.read_csv("MOVIE_Table.csv")
 
+error_ind = []
+
 def convert_date(show_datetime):
     dt = datetime.strptime(show_datetime, '%Y%m%d%H%M')
     return dt.date()
@@ -26,32 +28,47 @@ def convert_time(show_time):
     time = datetime.strptime(show_time, '%H%M').strftime('%I:%M %p')
     return time
 
-site = "https://in.bookmyshow.com/buytickets/veere-di-wedding-national-capital-region-ncr/movie-ncr-ET00042975-MT/20180615"
+for ind, site in enumerate(movie_table.booking_links):
 
-try:
-    print("Extracting Data, please wait...\n")
-    page = requests.get(site)
-except:
-    raise ConnectionError("Check your Internet Connection")
+    #if(ind == 47):
+    print(ind)
+    #site = "https://in.bookmyshow.com/buytickets/veere-di-wedding-national-capital-region-ncr/movie-ncr-ET00042975-MT/20180615"
     
-soup = BeautifulSoup(page.content, "html.parser")
-
-body = soup.find("body")
-
-showtimes = soup.find("section", {"class" : ["phpShowtimes", "showtimes"]})
-
-venues = showtimes.find_all("li", class_ = "list")
-
-for venue in venues:
-    theatre_table = theatre_table.append({"theatre_ID" : venue["data-id"], "theatre_name" : venue["data-name"], "theatre_lat" : venue["data-lat"], "theatre_long" : venue["data-lng"], "city" : venue["data-sub-region-name"], "city_ID" : venue["data-sub-region-id"]}, ignore_index = True)
-    
-    show_body = venue.find("div", class_ = "body ")
-    shows = show_body.find_all("div", {"data-online" : "Y"})
-    
-    for show_div in shows:
-        show = show_div.find("a")
-        show_table = show_table.append({"show_ID" : show["data-session-id"], "theatre_ID" : show["data-venue-code"], "movie_ID" : show["data-event-id"], "show_date" : show["data-cut-off-date-time"], "show_timing" : show["data-showtime-code"], "seat_percent_available" : show["data-seats-percent"]}, ignore_index = True)
-    
+    try:
+        print("Extracting Data, please wait...\n")
+        page = requests.get(site)
+    except:
+        try:
+            print("Retrying...\n")
+            page = requests.get(site)
+        except:
+            print("No connection!!..Skipping!")
+            error_ind.append(ind)
+            continue
+     
+    try:    
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        body = soup.find("body")
+        
+        showtimes = soup.find("section", {"class" : ["phpShowtimes", "showtimes"]})
+        
+        venues = showtimes.find_all("li", class_ = "list")
+        
+        for venue in venues:
+            theatre_table = theatre_table.append({"theatre_ID" : venue["data-id"], "theatre_name" : venue["data-name"], "theatre_lat" : venue["data-lat"], "theatre_long" : venue["data-lng"], "city" : venue["data-sub-region-name"], "city_ID" : venue["data-sub-region-id"]}, ignore_index = True)
+            
+            show_body = venue.find("div", class_ = "body ")
+            shows = show_body.find_all("div", {"data-online" : "Y"})
+            
+            for show_div in shows:
+                show = show_div.find("a")
+                show_table = show_table.append({"show_ID" : show["data-session-id"], "theatre_ID" : show["data-venue-code"], "movie_ID" : show["data-event-id"], "show_date" : show["data-cut-off-date-time"], "show_timing" : show["data-showtime-code"], "seat_percent_available" : show["data-seats-percent"]}, ignore_index = True)
+    except:
+        print("No movie")
+        error_ind.append(ind)
+        continue;
+         
 show_table.show_date = show_table.show_date.apply(convert_date)
 show_table.show_timing = show_table.show_timing.apply(convert_time)    
 
